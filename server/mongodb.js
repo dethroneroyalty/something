5b25ec3e3b731d1e5f99fb8166410b9916cb38d1
Oleg Tsyba <oleg.tsyba.ua@gmail.com>
2017-09-21 12:52:04 +0000

    chore: add litle refactor to Mongo connection

--- server/mongodb.js
+++ server/mongodb.js
@@ -3,11 +3,11 @@ const { MongoClient } = require("mongodb");
 let DB;
 
 module.exports = function mongoClient(url) {
-  if (DB) return Promise.resolve(DB);
-
-  DB = MongoClient.connect(url).catch(err => {
-    console.error(err.stack);
-    process.exit(1);
-  });
+  if (!DB) {
+    DB = MongoClient.connect(url).catch(err => {
+      console.error(err.stack);
+      process.exit(1);
+    });
+  }
   return DB;
 };
